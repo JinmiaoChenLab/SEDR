6999798615f17a25f3578b71a749a78f90294de4
Xuhang01 <qinghang.xu@gmail.com>
2023-10-09 09:47:26 +0800

    conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -27,10 +27,10 @@
 # ones.
 extensions = [
     "nbsphinx",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.autosummary",
-    "sphinx.ext.napoleon",
+    # "sphinx.ext.autodoc",
+    # "sphinx.ext.intersphinx",
+    # "sphinx.ext.autosummary",
+    # "sphinx.ext.napoleon",
 ]
 
 autosummary_generate = True
@@ -112,7 +112,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "alabaster"
+# html_theme = "alabaster"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
