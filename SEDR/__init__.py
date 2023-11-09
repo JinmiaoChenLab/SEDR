1de95ddbdb234252d33c78c01b8dcdb02e7626db
Xuhang01 <qinghang.xu@gmail.com>
2023-11-09 09:09:17 +0800

    Revise import

--- SEDR/__init__.py
+++ SEDR/__init__.py
@@ -1,7 +1,7 @@
 from .graph_func import graph_construction, combine_graph_dict
 from .utils_func import adata_preprocess, fix_seed
 from .SEDR_model import Sedr
-from .clustering_func import res_search_fixed_clus, mclust_R
+from .clustering_func import  mclust_R, leiden, louvain
 
 
 __all__ = [
